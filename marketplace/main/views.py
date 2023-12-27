aaf0ded00e6abe5f30524161055ffdc2f7d1d78b
salah eddine elhandaoui <salaheddine.elhandaoui@um5r.ac.ma>
2023-12-27 16:53:46 +0100

    add signup pages and form

--- marketplace/main/views.py
+++ marketplace/main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
-
+from .forms import SignupForm
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -14,3 +14,16 @@ def index(request):
 
 def contact(request):
     return render(request, 'main/contact.html')
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+        # return redirect('/login')
+    else:
+        form = SignupForm()
+    return render(request, 'main/signup.html', {'form':form})
+
